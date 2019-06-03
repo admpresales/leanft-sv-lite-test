c1ade875045e36cbf77181be263d10f56892f7aa
Admin Default <admin@default.com>
2019-06-03 17:53:00 -0400

    1. better handling proxy 2. added comments 3. make it better

--- src/main/java/com/mf/LeanFtTest.java
+++ src/main/java/com/mf/LeanFtTest.java
@@ -1,6 +1,7 @@
 package com.mf;
 
 import com.hp.lft.report.Status;
+import com.hp.lft.sdk.SvInfo;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -11,7 +12,7 @@ import com.hp.lft.sdk.web.*;
 import com.hp.lft.verifications.*;
 import com.hp.lft.report.Reporter;
 import com.hp.lft.report.ReportException;
-
+import com.hp.lft.sdk.Desktop;
 import unittesting.*;
 
 import java.awt.image.RenderedImage;
@@ -22,9 +23,9 @@ import java.util.Locale;
 public class LeanFtTest extends UnitTestClassBase {
     Browser browser;
     Boolean useProxy = true;
-    String proxyAddress = "http://localhost:9000";
+    //String proxyAddress = "http://localhost:9000";
     AOS_Model model;
-    BrowserType browserType = BrowserType.FIREFOX;
+    BrowserType browserType = BrowserType.CHROME;
 
     @BeforeClass
     public void beforeClass() throws Exception {
@@ -36,9 +37,12 @@ public class LeanFtTest extends UnitTestClassBase {
 
     @BeforeMethod
     public void beforeMethod() throws Exception {
-        BrowserDescription browserDescription = new BrowserDescription.Builder().type(browserType).build();
+        BrowserDescription browserDescription = new BrowserDescription();
+        browserDescription.setType(browserType);
+
         if (useProxy)
-            browserDescription.set("profile_proxy", proxyAddress);
+            browserDescription.set("profile_proxy", Desktop.getSvInfo().getSvHttpProxy()); // ("http://localhost:9000"
+
         browser = BrowserFactory.launch(browserDescription);
         model = new AOS_Model(browser);
     }
@@ -46,23 +50,31 @@ public class LeanFtTest extends UnitTestClassBase {
     @AfterMethod
     public void afterMethod() throws Exception {
         browser.close();
-
     }
 
     @Test
     public void test() throws GeneralLeanFtException, InterruptedException, ReportException {
+        // Navigate to the website
         browser.navigate("http://nimbusserver.aos.com:8000");
         browser.sync();
 
-        model.AdvantageShoppingPage().SPECIALOFFERLink().click();
-        model.AdvantageShoppingPage().SeeOfferBtnButton().click();
+        model.AdvantageShoppingPage().LAPTOPSShopNowLink().click();     // Click the laptops category
         browser.sync();
+        model.AdvantageShoppingPage().PinkLaptopWebElement().click();       // Select the pink laptop
+        browser.sync();
+        model.AdvantageShoppingPage().QuantityEditField().setValue("3");    // Set "3" in the amount field
 
-        comparePrice(449.99, model.AdvantageShoppingPage().PriceWebElement());
+        comparePrice(179.99, model.AdvantageShoppingPage().ActualLaptopPriceWebElement());      // Compare the
 
-        Thread.sleep(5000); // just wait for 5 seconds
+        model.AdvantageShoppingPage().SaveToCartButton().highlight();       // Highlight the Save to Cart object
+        model.AdvantageShoppingPage().SaveToCartButton().click();           // Click the Save to Cart object
+
+        Thread.sleep(5000); // just wait for 5 seconds          // Wait for 5 seconds before exiting test
     }
 
+    /*
+    * Compare an expected price to an actual price - taken from the object passed
+    */
     private void comparePrice(double expectedPrice, WebElement webElement) throws GeneralLeanFtException, ReportException {
         webElement.highlight();
 
@@ -79,6 +91,11 @@ public class LeanFtTest extends UnitTestClassBase {
         }
     }
 
+    /*
+    * Format a price to a double.
+    * Remove "$" sign and white spaces.
+    * Parse the "comma" format (x,xxx) to an actual number
+    */
     private double stringToDouble (String input)  {
         double retVal = 0;
         Number number;
